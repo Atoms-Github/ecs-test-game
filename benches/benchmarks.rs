5035a453aa69bc6474aa3577e2ad9b2c927ee7b1
Thomas <tomullender@gmail.com>
2022-11-03 16:24:41 +0000

    Added benchmark reports

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -1,7 +1,15 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
+#![allow(unused_imports)]
+#![allow(unused_mut)]
+#![allow(unused_attributes)]
+#![allow(non_camel_case_types)]
+#![allow(unused_parens)]
+
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 use ecs_test_game::game_legion::GameLegion;
-use ecs_test_game::main::GuiSettings;
+use ecs_test_game::rts::*;
 use std::time::Duration;
 
 criterion_group!(benches, rts_benchmark);
@@ -12,8 +20,18 @@ fn rts_benchmark(c: &mut Criterion) {
     const UNIVERSES: usize = 10;
     group.sample_size(10);
     group.measurement_time(Duration::from_secs(10));
-    group.warm_up_time(Duration::from_millis(20));
+    group.warm_up_time(Duration::from_millis(100));
     group.bench_function("legion", |b| {
+        // Init game state
+        let mut world = GameLegion::new();
+        for i in 0..UNIVERSES {
+            world.load_universe(i);
+        }
+        b.iter(move || {
+            world.update(1. / 60., &GuiSettings::default());
+        });
+    });
+    group.bench_function("legionagain", |b| {
         // Init game state
         let mut world = GameLegion::new();
         for i in 0..UNIVERSES {
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,11 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
+#![allow(unused_imports)]
+#![allow(unused_mut)]
+#![allow(unused_attributes)]
+#![allow(non_camel_case_types)]
+#![allow(unused_parens)]
+
 pub mod game_legion;
 pub mod gamedb;
 pub mod rts;
