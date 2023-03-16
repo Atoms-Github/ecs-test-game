7a79ed8cc61dd9036fb9e8139d5c0edaf607fd54
Thomas <tomullender@gmail.com>
2023-03-16 16:26:41 +0000

    Added query complex benchmark.

--- benches/bench_utils.rs
+++ benches/bench_utils.rs
@@ -1,13 +1,33 @@
 use std::fmt::format;
+use std::{env, path};
 
 use criterion::measurement::WallTime;
 use criterion::{BenchmarkGroup, BenchmarkId, Criterion};
 use ecs_test_game::simulation_settings::{BrainType, SimSettings};
 use ecs_test_game::test_controller::TestController;
+use ecs_test_game::MAP_SIZE;
+use ggez::Context;
 use rand::Rng;
+pub fn gen_test_ctx() -> Context {
+	let resource_dir = if let Ok(manifest_dir) = env::var("CARGO_MANIFEST_DIR") {
+		let mut path = path::PathBuf::from(manifest_dir);
+		path.push("resources");
+		path
+	} else {
+		path::PathBuf::from("./resources")
+	};
+	let mut cb = ggez::ContextBuilder::new("ECS Benchmark", "ggez").add_resource_path(resource_dir);
+
+	cb = cb.window_setup(ggez::conf::WindowSetup::default().title("Ecs Performance Benchmark"));
+	cb = cb.window_mode(ggez::conf::WindowMode::default().visible(false));
+
+	let (mut ctx, event_loop) = cb.build().unwrap();
+	ctx
+}
 
 pub fn benchmark(group: &mut BenchmarkGroup<WallTime>, settings: &SimSettings, frame_count: usize) {
-	let mut controller = TestController::gen_test_controller(settings);
+	let mut ctx = gen_test_ctx();
+	let mut controller = TestController::gen_test_controller(&mut ctx, settings);
 	let mut entity_count = settings.entity_count;
 
 	let name = format!("{}, {}", settings.brain_type.to_string(), rand::thread_rng().gen_range(0..5000));
--- benches/query_complex.rs
+++ benches/query_complex.rs
@@ -6,31 +6,37 @@
 #![allow(non_camel_case_types)]
 #![allow(unused_parens)]
 
+mod bench_utils;
 use std::time::Duration;
 
 use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
-use ecs_test_game::brains::brain_legion::BrainLegion;
+use ecs_test_game::brains::brain_legion::{BrainLegion, BrainLegionCounted};
 use ecs_test_game::challenges::rts::ChallengeRts;
 use ecs_test_game::simulation_settings::{BrainType, Challenge, SimSettings};
 use ecs_test_game::test_controller::TestController;
 
-criterion_group!(benches, nearest_line);
+use crate::bench_utils::benchmark;
+
+criterion_group!(benches, query_complex);
 criterion_main!(benches);
 
-fn spatial_array(c: &mut Criterion) {
-	let mut group = c.benchmark_group("spatial_array");
+fn query_complex(c: &mut Criterion) {
+	let mut group = c.benchmark_group("query_complex");
 	group.sample_size(10);
 	group.measurement_time(Duration::from_secs(3));
 	group.warm_up_time(Duration::from_millis(100));
 
-	let tests = [BrainType::LegionDupey, BrainType::SqlDuck];
-	let entity_counts = [5, 20, 50, 100, 500, 1000];
 	let mut settings = SimSettings::default();
+	settings.challenge_type = Challenge::QueryChallenge;
 
-	settings.challenge_type = Challenge::SpacialArray {};
-	for entity_count in entity_counts {
+	for entity_count in (1..2).map(|x| x * 2) {
 		settings.entity_count = entity_count;
-		for test in tests {
+		for test in [
+			BrainType::LegionDupey,
+			BrainType::LegionCounted,
+			BrainType::SqlDuck,
+			BrainType::SqlIte,
+		] {
 			settings.brain_type = test;
 			benchmark(&mut group, &settings, 3);
 		}
